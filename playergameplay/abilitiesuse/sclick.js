0eb79b2f7bf921e6993edb37a6f852f2caabaf23
Ag <awesomeag61@gmail.com>
2022-02-23 11:05:21 -0800

    added base code for fireballs + water shoot on s click

--- playergameplay/abilitiesuse/sclick.js
+++ playergameplay/abilitiesuse/sclick.js
@@ -2,17 +2,29 @@
 const tailslapuse = require('../../entity/abilitys/kdtailslap')
 
 const utils1 = require("../../modules/IMPmodules/util")
+const fireuse = require("../../entity/abilitys/fire/fireballuse")
 
 const util = new utils1()
 function abilities(aobjids, player, entities) {
     if (entities[player]) {
+        // if (entities[player].bar.normalbar !=2) {}
+       if (entities[player].bar.normalbar == 0) {
+            if (entities[player].bar.normalbarpercentage > 6.0){
+                
+            }
+        } 
+        else if (entities[player].bar.normalbar == 2) {
+            if (entities[player].bar.normalbarpercentage > 6.0) {
+                new fireuse(aobjids, 0, entities, entities[player].id, entities[player].specType2)
+                entities[player].bar.normalbarpercentage -= 6.0
+                console.log(entities[player].bar.barpercentage)
+            }
+        }
+        else {
 
-        if (entities[player].bar.normalbar != 2) {
-
-        } else {
 
             if (entities[player].secondaryType == 79) {
-                if (entities[player].canUseTailslap && entities[player].tailState == 0) {
+                if (entities[player].canUseTailslap && (entities[player].tailState == 0 || entities[player].infability) && entities[player].arenaid == 0) {
 
                     entities[player].tailState = 100
                     entities[player].canUseTailslap = false
