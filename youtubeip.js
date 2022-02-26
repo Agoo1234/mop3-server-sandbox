bf6d0a1be2ecce92f4fd802b31c85e75566a2a46
Ag <awesomeag61@gmail.com>
2022-02-25 22:14:33 -0800

    removed devs as ytbers

--- youtubeip.js
+++ youtubeip.js
@@ -1,6 +1,6 @@
 function youtubeip(ws) {
 
-	if (ws._socket.remoteAddress == '::1' || ws._socket.remoteAddress == '::ffff:127.0.0.1' || ws._socket.remoteAddress == '::ffff:98.47.209.188') {
+	if (ws._socket.remoteAddress == '::1') {
 		{
 			ws.isyoutuber = true
 			//PLAYER IS YouTuber
