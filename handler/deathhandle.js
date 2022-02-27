c2f3b948bd1a53510ca4c8883fa0a37d3dfdaac9
Ag <awesomeag61@gmail.com>
2022-02-26 17:50:54 -0800

    silenced errors

--- handler/deathhandle.js
+++ handler/deathhandle.js
@@ -5,9 +5,9 @@ function deathhandle(entities, player2, player1) {
     try {
         if (entities[player2].hp < 0.01) {
 
-            entities[player2].killerid = entities[player1].id
+            try {entities[player2].killerid = entities[player1].id} catch(err) {}
             entities[player1].lasthp = entities[player1].maxhp 
-            try {entities[player1].hp += entities[player1].maxhp / 6 } catch(err) {console.log(error)}
+            try {entities[player1].hp += entities[player1].maxhp / 6 } catch(err) {}
             entities[player1].kills++
             entities[player1].xp += entities[player2].xp / 1.5
             entities[player1].barpercentage = 100
