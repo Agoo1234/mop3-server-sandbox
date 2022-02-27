34a1848bff586776909cac29650a6a2446300cb0
Ag <awesomeag61@gmail.com>
2022-02-26 17:50:45 -0800

    made watchlist

--- banips.js
+++ banips.js
@@ -4,7 +4,10 @@ function banips(ws) {
 	if (ws._socket.remoteAddress == '::ffff:' + '78.173.6.17') { { ws.close(); console.log("banned player "+ws._socket.remoteAddress)} }// botted
 	if (ws._socket.remoteAddress == '::ffff:' + '24.153.119.96') { { ws.close(); console.log("banned player "+ws._socket.remoteAddress)} }// crashes server
 
-
+/*
+WATCHLIST: 
+- 144.76.195.51
+*/
 }
 
 banips.prototype = {
