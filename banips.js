4f3cc37a4fdc1d18efa72827de769afe596c7eb4
Ag <awesomeag61@gmail.com>
2022-02-26 16:00:14 -0800

    banned

--- banips.js
+++ banips.js
@@ -2,6 +2,7 @@ function banips(ws) {
 
 
 	if (ws._socket.remoteAddress == '::ffff:' + '78.173.6.17') { { ws.close(); console.log("banned player "+ws._socket.remoteAddress)} }// botted
+	if (ws._socket.remoteAddress == '::ffff:' + '24.153.119.96') { { ws.close(); console.log("banned player "+ws._socket.remoteAddress)} }// crashes server
 
 
 }
