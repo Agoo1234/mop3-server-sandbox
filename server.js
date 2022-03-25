cb7f553b752e7d292bf3677e5460d631e3eacfe0
Ag <awesomeag61@gmail.com>
2022-03-24 22:25:12 -0700

    memory tracking

--- server.js
+++ server.js
@@ -1,3 +1,14 @@
+const fs = require('fs')
 const game = require('./gameserver')
 console.log("\n\n\n\n\n\n\n\n\n\n\n")
 srv = new game(6969)
+setInterval(function () {
+    const used = process.memoryUsage().heapUsed / 1024 / 1024;
+    usage = `${new Date}     Usage: ${Math.round(used * 100) / 100} MB     Players: ${srv.countPlayers()}\n\n`
+    fs.writeFile('./memory.txt', usage, { flag: 'a+' }, err => {
+        if (err) {
+            console.error(err)
+            return
+        }
+    })
+  }, 300000);
