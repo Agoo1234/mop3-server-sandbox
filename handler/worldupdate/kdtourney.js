57ee69817074184ada95db1b499fe4a6f1770a18
Ag <awesomeag61@gmail.com>
2022-03-24 22:24:45 -0700

    Create kdtourney.js

---
+++ handler/worldupdate/kdtourney.js
@@ -0,0 +1,13 @@
+const arena = require("../../entity/objects/objects/arena")
+
+
+
+function kdtourney(aobjids, ws_new, entities, players) {
+    
+}
+
+kdtourney.prototype = {
+
+}
+
+module.exports = kdtourney
