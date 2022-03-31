01e3987ccf7edf99dd2927faa0a03882f0beb91e
Ag <awesomeag61@gmail.com>
2022-03-30 17:58:03 -0700

    fix cpu usage

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ console.log("\n\n\n\n\n\n\n\n\n\n\n")
 srv = new game(6969)
 setInterval(function () {
     var used = process.memoryUsage().heapUsed / 1024 / 1024;
-    usage = `${new Date}     Memory: ${Math.round(used * 100) / 100} MB     CPU Load: ${Math.round(100*os.loadavg)/100}     Players: ${srv.countPlayers()}\n\n`
+    usage = `${new Date}     Memory: ${Math.round(used * 100) / 100} MB     CPU Load: ${Math.round(100*os.loadavg()[0])/100}     Players: ${srv.countPlayers()}\n\n`
     fs.writeFile('./memory.txt', usage, { flag: 'a+' }, err => {
         if (err) {
             console.error(err)
